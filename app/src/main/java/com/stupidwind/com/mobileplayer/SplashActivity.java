4151b6d9ae45adcf73aa9faf1c05d430a70d850b
xfsero <xing944291293@vip.qq.com>
2017-03-21 00:23:33 +0800

    Update SplashActivity

--- app/src/main/java/com/stupidwind/com/mobileplayer/SplashActivity.java
+++ app/src/main/java/com/stupidwind/com/mobileplayer/SplashActivity.java
@@ -12,15 +12,17 @@ public class SplashActivity extends Activity {
 
     private static final String TAG = SplashActivity.class.getSimpleName();
 
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                //执行主线程
+                //执行在主线程中
                 Log.d(TAG, "当前线程名称 = " + Thread.currentThread().getName());
                 startMainActivity();
             }
@@ -38,6 +40,8 @@ public class SplashActivity extends Activity {
             isStartMain = true;
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+
+            //关闭当前Activity
             finish();
         }
     }
@@ -47,5 +51,12 @@ public class SplashActivity extends Activity {
         Log.e(TAG, "当前事件名称 = Action" + event.getAction());
         startMainActivity();
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        //把所有消息和回调移除
+        handler.removeCallbacksAndMessages(null);
+        super.onDestroy();
     }
 }
